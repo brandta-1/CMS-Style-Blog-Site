2df0257d3ee78b528b3fa4b4c637a426e45fc2d5
Alex Brandt <alexezemail@gmail.com>
2023-05-15 13:46:31 -0400

    postpage needed logged_in bool

--- C/C-postpage.js
+++ C/C-postpage.js
@@ -28,7 +28,7 @@ router.get('/:id', withAuth, async (req, res) => {
             isOwner: Boolean(i.user_id == req.session.user_id)
         }))
 
-        res.render('postpage', { ...post, owner });
+        res.render('postpage', { ...post, owner, logged_in: req.session.logged_in });
 
     } catch (err) {
         res.status(500).json(err);
